2f51c1e64d5ebf7c3c14cbbe1e62450be6de74ba
PrasanMadhikarmi <prasanmadhikarmi7@gmail.com>
2022-03-28 17:56:11 +0545

    Changed UI of index page

--- hrm/index.php
+++ hrm/index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="../CSS/frontpage.css">
 </head>
 <body>
-    <header>
+    <!-- <header>
         <main>
             <div class="left">
                 <h3>Welcome to........</h3>
@@ -17,6 +17,15 @@
                 <a href="../HTML/main.php" class="btn1">Get Started!</a>
             </div>
         </main>
-    </header>
+    </header> -->
+    <div class="index-ctn">
+        <div class="opaque-bg"></div>
+        <img src="../assets/main1.jpg" alt="">
+        <div class="title-ctn">
+            <h1>Welcome to Quanite Creations</h1>
+            <h5>A Human Resource Management System</h5>  
+        </div>
+        <a href="../HTML/main.php"><button class="btn"><span>Get Started!</span></button></a>
+    </div>
 </body>
 </html>
